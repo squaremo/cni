e0a9623d96075de6764f02cecd3ca7ec776dd07a
Michael Bridgen <mikeb@squaremobius.net>
2015-08-17 13:35:03 +0100

    Move argument-handling and conf-loading into public API
    The fact that the arguments get turned into an environment for the
    plugin is incidental to the API; so, provide a way of supplying them
    as a struct or saying "just use the same arguments as I got" (the
    latter is for IPAM plugins).
    
    It's also worth having some mix-and-match procedures for finding and
    loading configuration from the filesystem, since applications will
    want to have different conventions. (These could probably go in their
    own pkg/.)

--- api.go
+++ api.go
@@ -1,10 +1,6 @@
 package cni
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"os"
 	"strings"
 
 	"github.com/appc/cni/pkg/invoke"
@@ -15,24 +11,12 @@ type RuntimeConf struct {
 	ContainerID string
 	NetNS       string
 	IfName      string
-	Args        string
+	Args        [][2]string
 }
 
 type NetworkConfig struct {
 	types.NetConf
 	Bytes []byte
-}
-
-func ConfFromFile(filename string) (*NetworkConfig, error) {
-	bytes, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, fmt.Errorf("error reading %s: %s", filename, err)
-	}
-	conf := &NetworkConfig{Bytes: bytes}
-	if err = json.Unmarshal(bytes, conf); err != nil {
-		return nil, fmt.Errorf("error parsing %s: %s", filename, err)
-	}
-	return conf, nil
 }
 
 type CNI interface {
@@ -55,29 +39,16 @@ func (c *CNIConfig) DelNetwork(net *NetworkConfig, rt *RuntimeConf) error {
 
 // =====
 
-// there's another in cni/pkg/plugin/ipam.go, but it assumes the
-// environment variables are inherited from the current process
 func (c *CNIConfig) execPlugin(action string, conf *NetworkConfig, rt *RuntimeConf) (*types.Result, error) {
 	pluginPath := invoke.FindInPath(conf.Type, c.Path)
 
-	vars := [][2]string{
-		{"CNI_COMMAND", action},
-		{"CNI_CONTAINERID", rt.ContainerID},
-		{"CNI_NETNS", rt.NetNS},
-		{"CNI_ARGS", rt.Args},
-		{"CNI_IFNAME", rt.IfName},
-		{"CNI_PATH", strings.Join(c.Path, ":")},
+	args := &invoke.Args{
+		Command:     action,
+		ContainerID: rt.ContainerID,
+		NetNS:       rt.NetNS,
+		PluginArgs:  rt.Args,
+		IfName:      rt.IfName,
+		Path:        strings.Join(c.Path, ":"),
 	}
-	return invoke.ExecPlugin(pluginPath, conf.Bytes, envVars(vars))
-}
-
-// taken from rkt/networking/net_plugin.go
-func envVars(vars [][2]string) []string {
-	env := os.Environ()
-
-	for _, kv := range vars {
-		env = append(env, strings.Join(kv[:], "="))
-	}
-
-	return env
+	return invoke.ExecPlugin(pluginPath, conf.Bytes, args)
 }
